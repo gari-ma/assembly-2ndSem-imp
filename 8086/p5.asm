0e16a2b4fd2b8ebd9d706a899cb38997d41975bc
imp-sike <93368594+imp-sike@users.noreply.github.com>
2022-04-20 11:31:53 +0545

    Comments are required

--- 8086/p5.asm
+++ 8086/p5.asm
@@ -1,1 +1,22 @@
 ; Program to display character with attribute
+
+.MODEL small
+.STACK
+.DATA
+.CODE
+MAIN PROC
+    MOV AX,@DATA
+    MOV DS,AX
+
+    MOV AH,09h
+    MOV AL, 01h
+    MOV BH, 00
+    MOV BL, 0c
+    MOV CX, 79
+    INT 20h
+    MOV AX, 4c00h
+    INT 21h
+
+    INT 21h
+MAIN ENDP
+END MAIN
